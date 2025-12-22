953f5415ea230607c4473dc61531b43785f40024
unknown <td6d@hotmail.com>
2025-12-22 23:16:36 +0100

    first version of react frontpage

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import { useNavigate } from "react-router-dom";
+
+const buttons = [
+  { title: "Recepies", image: "/images/recepies.jpg", path: "/recepies" },
+  { title: "Panels", image: "/images/panels.jpg", path: "/panels" },
+  { title: "Pictures", image: "/images/pictures.jpg", path: "/pictures" },
+  { title: "Code", image: "/images/code.jpg", path: "/code" },
+];
+
+export default function App() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      {buttons.map(({ title, image, path }) => (
+        <button
+          key={path}
+          className="tile"
+          style={{ backgroundImage: `url(${image})` }}
+          onClick={() => navigate(path)}
+        >
+          <span>{title}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+
+const Page = ({ title }: { title: string }) => (
+  <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontSize: "3rem",
+    backgroundColor: "#222",
+    color: "white"
+  }}>
+    {title}
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/recepies" element={<Page title="Recepies" />} />
+      <Route path="/panels" element={<Page title="Panels" />} />
+      <Route path="/pictures" element={<Page title="Pictures" />} />
+      <Route path="/code" element={<Page title="Code" />} />
+    </Routes>
+  </BrowserRouter>
+);
